af8b903e36a4a6ddacc7e412acbaaacfb3803458
helred1221 <helder.pereira@estudante.ifms.edu.br>
2025-05-11 15:12:48 -0400

    fix: added some comments to better urdentand the code.

--- tmp/bancada1.ts
+++ tmp/bancada1.ts
@@ -2,13 +2,17 @@ import { loadPackageDefinition, Server, ServerCredentials, status, sendUnaryData
 import { loadSync } from '@grpc/proto-loader';
 import { retornaValorRandom } from './utils';
 
-
+// Bancada package definition variable instantiation.
 const bancadaDefs = loadSync('./protos/bancada.proto');
+// The bancada package definition Load as a gRPC object hierarchy. 
 const bancadaProto = loadPackageDefinition(bancadaDefs) as any;
 
+// Creation of a new Bancada Server
 const bancadaServer = new Server();
 
+// An implementation of the Bancada Service is added here
 bancadaServer.addService(bancadaProto.BancadaService.service, {
+  // This is the implementation of the service definition on proto bancada file passing random values to the client through the proto buffers
   GetDados: (_: any, callback: any) => {
     callback(null, {
       id: 1,
@@ -19,6 +23,7 @@ bancadaServer.addService(bancadaProto.BancadaService.service, {
   }
 });
 
+// Aqui é criado o bindAsync do servidor para executar qualquer IP, na porta 50051 passando credenciais inseguras como parâmetro. 
 bancadaServer.bindAsync('0.0.0.0:50051', ServerCredentials.createInsecure(), () => {
   console.log('Bancada 1 rodando em 0.0.0.0:50051');
 });
